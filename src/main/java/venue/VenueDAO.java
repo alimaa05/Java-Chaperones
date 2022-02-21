70db427ad67d8a285a70138222765d0d1d1acf0f
Andrew Winebrenner <andrewjwinebrenner@gmail.com>
2022-02-21 15:19:25 +0000

    add venueSQL methods

--- src/main/java/venue/VenueDAO.java
+++ src/main/java/venue/VenueDAO.java
@@ -5,9 +5,9 @@ import user.User;
 import java.util.List;
 
 public interface VenueDAO {
-    public void add(Venue venue);
+    public int add(Venue venue);
     public List<Venue> getAll();
     public Venue getById(int id);
-    public void updateById(int id, Venue update);
-    public void deleteById(int id);
+    public int updateById(int id, Venue update);
+    public int deleteById(int id);
 }
--- src/main/java/venue/VenueSQL.java
+++ src/main/java/venue/VenueSQL.java
@@ -1,21 +1,84 @@
 package venue;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository("postgres")
 public class VenueSQL implements VenueDAO{
-    public void add(Venue venue){
 
+    private JdbcTemplate jdbcTemplate;
+
+    public VenueSQL(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public int add(Venue venue){
+
+        String sql = "INSERT INTO venues (name, area, address) VALUES(?, ?, ?)";
+
+        return jdbcTemplate.update(sql,
+            venue.getName(),
+            venue.getArea(),
+            venue.getAddress()
+        );
     };
+
     public List<Venue> getAll(){
 
+        String sql = "SELECT id, name, area, address FROM venues";
+
+        return jdbcTemplate.query(sql, (rs, rowNum) ->
+            new Venue(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("area"),
+                rs.getString("address")
+            )
+        );
     };
+
     public Venue getById(int id){
 
-    };
-    public void updateById(int id, Venue update){
+        String sql = "SELECT id, name, area, address FROM venues WHERE id = ?";
+
+        return jdbcTemplate.queryForObject(sql, (rs, rowNum) ->
+            new Venue(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("area"),
+                rs.getString("address")
+            ),
+            id
+        );
 
     };
-    public void deleteById(int id){
+    public int updateById(int id, Venue update){
 
+        String sql = "UPDATE venues SET (name, area, address)=(?, ?, ?) WHERE id = ?";
+
+        Venue original = getById(id);
+
+        String newName = update.getName();
+        if (newName == null) newName = original.getName();
+        String newArea = update.getArea();
+        if (newArea == null) newArea = original.getArea();
+        String newAddress = update.getAddress();
+        if (newAddress == null) newAddress = original.getAddress();
+
+        return jdbcTemplate.update(sql,
+                newName,
+                newArea,
+                newAddress,
+                id
+        );
+
+    };
+    public int deleteById(int id){
+
+        String sql = "DELETE FROM venues WHERE id = ?";
+
+        return jdbcTemplate.update(sql, id);
     };
 }
