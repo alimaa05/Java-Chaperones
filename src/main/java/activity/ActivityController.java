d83e6162a46cfd252e35bca8cdbc9572bcd38fa5
Andrew Winebrenner <andrewjwinebrenner@gmail.com>
2022-02-21 17:36:48 +0000

    add db name in resources

--- src/main/java/activity/ActivityController.java
+++ src/main/java/activity/ActivityController.java
@@ -1,4 +1,7 @@
 package activity;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ActivityController {
 }
--- src/main/java/guide/GuideController.java
+++ src/main/java/guide/GuideController.java
@@ -1,4 +1,7 @@
 package guide;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GuideController {
 }
