588b5fe13b895806848d96b2562a2a25ea017b66
Andrew Winebrenner <andrewjwinebrenner@gmail.com>
2022-02-21 16:57:38 +0000

    add venue controller

--- src/main/java/venue/VenueController.java
+++ src/main/java/venue/VenueController.java
@@ -1,4 +1,41 @@
 package venue;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class VenueController {
+
+    private VenueService venueService;
+
+    public VenueController(VenueService venueService) {
+        this.venueService = venueService;
+    }
+
+    @PostMapping(path = "venues")
+    public void addNewVenue(@RequestBody Venue venue){
+        venueService.addNewVenue(venue);
+    }
+
+    @GetMapping(path = "venues")
+    public List<Venue> getAllVenues() {
+        return venueService.getAllVenues();
+    }
+
+    @GetMapping(path = "venues/{id}")
+    public Venue getVenueById(@PathVariable("id") Integer id) {
+        return venueService.getVenueById(id);
+    }
+
+    @PutMapping(path = "venues/{id}")
+    public void updateVenueById(@PathVariable("id") Integer id, @RequestBody Venue update) {
+        venueService.updateVenueById(id, update);
+    }
+
+    @DeleteMapping(path = "venues/{id}")
+    public void deleteVenueById(@PathVariable("id") Integer id) {
+        venueService.deleteVenueById(id);
+    }
+
 }
