aa28fd2d0413e390fe9df9caff6e1a3cd8334ec4
Alimaa <alimaahashim@gmail.com>
2022-02-21 15:22:50 +0000

    add activitySQL add method

--- src/main/java/activity/ActivityDAO.java
+++ src/main/java/activity/ActivityDAO.java
@@ -6,7 +6,7 @@ import venue.Venue;
 import java.util.List;
 
 public interface ActivityDAO {
-    public void add(Activity activity, Guide guide, Venue venue);
+    public int add(Activity activity, Guide guide, Venue venue);
     public List<Activity> getAll();
     public Activity getById(int id);
     public void updateById(int id, Activity update);
--- src/main/java/activity/ActivitySQL.java
+++ src/main/java/activity/ActivitySQL.java
@@ -1,14 +1,48 @@
 package activity;
 
 import guide.Guide;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import venue.Venue;
 
 import java.util.List;
 
+@Repository("postgres")
 public class ActivitySQL implements ActivityDAO{
-    public void add(Activity activity, Guide guide, Venue venue){
 
-    };
+    private JdbcTemplate jdbcTemplate;
+
+    public ActivitySQL(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // Method to add an activity
+    @Override
+    public int add(Activity activity, Guide guide, Venue venue){
+        String sql = """
+                INSERT INTO activities (guide_id, venue_id, name, description, date, time, duration, price, capacity)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                """;
+
+        int rowAffected = jdbcTemplate.update(
+                sql,
+                guide.getId(),
+                venue.getId(),
+                activity.getName(),
+                activity.getDescription(),
+                activity.getDate(),
+                activity.getTime(),
+                activity.getDuration(),
+                activity.getPrice(),
+                activity.getCapacity()
+
+        );
+        return rowAffected;
+
+    }
+
+    // ----------------------------------------------------------
+
     public List<Activity> getAll(){
 
     };
