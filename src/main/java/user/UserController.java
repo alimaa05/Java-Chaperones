4fc700c8eacc0a12bf9859e985ddde3015936ff3
Adib Baig <adibzbaig@gmail.com>
2022-02-21 17:26:54 +0000

    UserController finished

--- src/main/java/user/UserController.java
+++ src/main/java/user/UserController.java
@@ -1,4 +1,40 @@
 package user;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(path = "users")
+    public void addUsers(@RequestBody User user) {
+        userService.addNewUser(user);
+    }
+
+    @GetMapping(path = "users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping(path = "users/{id}")
+    public User getUserById(@PathVariable("id") Integer id) {
+        return userService.getUserById(id);
+    }
+
+    @PutMapping(path = "users/{id}")
+    public void updateUserById(@PathVariable("id") Integer id, @RequestBody User update) {
+        userService.updateUserById(id, update);
+    }
+
+    @DeleteMapping(path = "users/{id}")
+    public void deleteUserById(@PathVariable("id") Integer id) {
+        userService.deleteUserById(id);
+    }
 }
--- src/main/java/user/UserDAO.java
+++ src/main/java/user/UserDAO.java
@@ -1,6 +1,5 @@
 package user;
 
-
 import java.util.List;
 
 public interface UserDAO {
