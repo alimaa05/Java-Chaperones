7b3513f039da518e7a037ccf4cb74c8597c45979
Andrew Winebrenner <andrewjwinebrenner@gmail.com>
2022-02-22 09:26:32 +0000

    start testing branch

---
+++ src/test/java/com/chaperones/venue/VenueSQLTest.java
@@ -0,0 +1,53 @@
+package com.chaperones.venue;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VenueSQLTest {
+
+    @Test
+    void add() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void getAll() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void getById() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void updateById() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void deleteById() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
