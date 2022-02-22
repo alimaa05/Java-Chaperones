9ee76b43acedd9fbd0b24fc2c053dd9e50d7f0d2
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-02-22 12:09:24 +0000

    added controlled to add a guide

--- src/main/java/com/chaperones/guide/GuideController.java
+++ src/main/java/com/chaperones/guide/GuideController.java
@@ -1,7 +1,24 @@
 package com.chaperones.guide;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class GuideController {
+
+    private GuideService guideService;
+
+    public GuideController(GuideService guideService) {
+        this.guideService = guideService;
+    }
+
+    @PostMapping("guide")
+    public void addGuide(@RequestBody Guide guide){
+        guideService.addGuide(guide);
+    }
+
+
 }
