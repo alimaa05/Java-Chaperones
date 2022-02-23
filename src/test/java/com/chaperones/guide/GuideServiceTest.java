8a614ed7a0a086a672712182817addb0dc80d41a
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-02-23 12:37:08 +0000

    add test for adding a guide

--- src/test/java/com/chaperones/guide/GuideServiceTest.java
+++ src/test/java/com/chaperones/guide/GuideServiceTest.java
@@ -1,13 +1,88 @@
 package com.chaperones.guide;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class GuideServiceTest {
+//    @InjectMocks
+//    GuideService underTest;
+//
+//    @Mock
+//    GuideDAO mockdao;
+//
+//    @BeforeEach
+//    public void init() {
+//        MockitoAnnotations.openMocks(underTest);
+//
+//    }
+    private GuideService underTest;
+    private GuideDAO mockDAO;
+
+    @BeforeEach
+    void setUp() {
+        mockDAO = Mockito.mock(GuideDAO.class);
+        underTest = new GuideService(mockDAO);
+    }
+        @Test
+    void addGuide() {
+        // Given
+        Guide testGuide = new Guide(1, "Colin", "01424 346816", "colin@gmail.com");
+
+        // When
+           when(mockDAO.add(testGuide)).thenReturn(1);
+           underTest.addGuide(testGuide);
+        // Then
+        verify(mockDAO, times(1)).add(testGuide);
+    }
 
     @Test
-    void addGuide() {
+    void allGuides() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void guideById() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void updateGuide() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void deleteGuide() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void guidesActivities() {
         // Given
 
         // When
