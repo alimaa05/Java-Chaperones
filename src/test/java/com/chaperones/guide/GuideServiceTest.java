4653078ffb6e06f5b3b67fe65d5495f08c3fd04c
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-02-23 16:28:20 +0000

    added two test methods for adding a guide

--- src/test/java/com/chaperones/guide/GuideServiceTest.java
+++ src/test/java/com/chaperones/guide/GuideServiceTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -36,13 +37,36 @@ class GuideServiceTest {
         @Test
     void addGuide() {
         // Given
-        Guide testGuide = new Guide(1, "Colin", "01424 346816", "colin@gmail.com");
+        Guide testGuide = new Guide(1, "blah", "01424 346816", "blah@gmail.com");
+        List<Guide> testList = new ArrayList<>(Arrays.asList(testGuide));
 
         // When
-           when(mockDAO.add(testGuide)).thenReturn(1);
+            when(mockDAO.getAll()).thenReturn(testList);
+            when(mockDAO.add(any())).thenReturn(1);
            underTest.addGuide(testGuide);
         // Then
         verify(mockDAO, times(1)).add(testGuide);
+    }
+    @Test
+    void addGuideWhenPhoneNumberAndEmailIsSameAsExistingGuides() {
+        // Given
+        List<Guide> testList = new ArrayList<>();
+        Guide testGuide = new Guide(1, "blah", "01424 346816", "blah@gmail.com");
+        testList.add(testGuide);
+        Guide addedGuide = new Guide(2, "la", "01424 346816", "blah@gmail.com");
+
+        // When
+        //doThrow(IllegalStateException.class).when(mockDAO).add(addedGuide);
+        //when(mockDAO.add(addedGuide)).thenThrow(IllegalArgumentException.class);
+        when(mockDAO.getAll()).thenReturn(testList);
+        when(mockDAO.add(any())).thenReturn(1);
+        IllegalStateException ex = assertThrows(IllegalStateException.class, () -> {
+            underTest.addGuide(addedGuide);
+        });
+        // Then
+        assertEquals("Guide already exists", ex.getMessage());
+        verify(mockDAO, never()).add(addedGuide);
+
     }
 
     @Test
